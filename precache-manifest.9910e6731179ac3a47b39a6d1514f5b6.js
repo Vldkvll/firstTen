3f886771b9a99c0edd5c750dff36e51e8ddab974
vldkvll <vldkvll.gmail.com>
2020-04-14 15:31:42 +0300

    Updates

---
+++ precache-manifest.9910e6731179ac3a47b39a6d1514f5b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65cfe64fce68c58487f9141d46dacf84",
+    "url": "/firstTen/index.html"
+  },
+  {
+    "revision": "a95f5b777753faacd9d3",
+    "url": "/firstTen/static/css/main.aafe97fe.chunk.css"
+  },
+  {
+    "revision": "b8e4f503db2687271f2a",
+    "url": "/firstTen/static/js/2.e9eda3d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/firstTen/static/js/2.e9eda3d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a95f5b777753faacd9d3",
+    "url": "/firstTen/static/js/main.9e0d1d2e.chunk.js"
+  },
+  {
+    "revision": "234e5f14b6eae33efec8",
+    "url": "/firstTen/static/js/runtime-main.4f7c43f7.js"
+  }
+]);
